3d4dff85ebde06f996b640a94409b8874e97a9be
Boobalan AP <apboobalan@gmail.com>
2020-07-25 13:08:40 +0530

    Sign in component test.

--- src/test/components/auth.test.js
+++ src/test/components/auth.test.js
@@ -11,4 +11,13 @@ describe("Auth component", () => {
     screen.getByLabelText("Email :");
     screen.getByLabelText("Password :");
   });
+
+  test("renders signin page without name field", () => {
+    render(<Auth type={"signIn"} />);
+
+    expect(screen.queryByText("Name :")).not.toBeInTheDocument();
+    screen.getByText("Sign In");
+    screen.getByLabelText("Email :");
+    screen.getByLabelText("Password :");
+  });
 });
