c4c4bbf919fc7676db708e5124331797e33e1afd
Boobalan AP <apboobalan@gmail.com>
2020-07-25 13:26:00 +0530

    Sign in button click test.

--- src/test/components/auth.test.js
+++ src/test/components/auth.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Auth from "../../components/Auth";
 
 describe("Auth component", () => {
@@ -19,5 +20,22 @@ describe("Auth component", () => {
     screen.getByText("Sign In");
     screen.getByLabelText("Email :");
     screen.getByLabelText("Password :");
+  });
+
+  test("renders signin page with a button name Signin and on click calls a handler with signIn data", async () => {
+    const onClickHandler = jest.fn();
+    render(<Auth type={"signIn"} onClickHandler={onClickHandler} />);
+
+    const signInButton = screen.getByTestId("sign-in-button");
+    userEvent.type(screen.getByTestId("email", "booda@booda.com"));
+    userEvent.type(screen.getByTestId("password", "..."));
+    userEvent.click(signInButton);
+
+    await expect(onClickHandler).toHaveBeenCalledTimes(1);
+    await expect(onClickHandler).toBeCalledWith({
+      type: "signIn",
+      email: "booda@booda.com",
+      password: "...",
+    });
   });
 });
