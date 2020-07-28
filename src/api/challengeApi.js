99f652cb796d113641594b1f42419c3b73c3f8ce
Boobalan AP <apboobalan@gmail.com>
2020-07-28 14:39:18 +0530

    Add environment variable for docker.

--- src/api/challengeApi.js
+++ src/api/challengeApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:4000";
+const URL = process.env.BACKEND_URL || "http://localhost:4000";
 
 export const signUp = async (authData) => {
   try {
