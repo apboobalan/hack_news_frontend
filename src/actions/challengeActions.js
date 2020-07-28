d14e8026f0220f178052e393b372bde6302d20c4
Boobalan AP <apboobalan@gmail.com>
2020-07-28 06:34:48 +0530

    Reorder and refactor actions and api.

--- src/actions/challengeActions.js
+++ src/actions/challengeActions.js
@@ -13,32 +13,6 @@ import {
   createChallenge,
 } from "../api/challengeApi";
 
-export const navigateToDetailsAction = (id) => async (dispatch, getState) => {
-  const jwt = getState().userReducer.jwt;
-  const challengeDetails = await getChallengeDetails(id, jwt);
-  challengeDetails.error
-    ? dispatch({ type: "SET_INFO", info: challengeDetails.error })
-    : dispatch({ type: NAVIGATE_TO_CHALLENGE_DETAILS, challenge: challengeDetails.data });
-};
-
-export const getChallengeListAction = () => async (dispatch, getState) => {
-  const user = JSON.parse(localStorage.getItem("user"))
-  const jwt = getState().userReducer.jwt || (user && user.jwt);
-  const sortByData = { type: getState().challengeListReducer.sort };
-  if (jwt) {
-    const challengeList = await getChallengeList(jwt, sortByData);
-    challengeList.error
-    ? dispatch({type: "SET_INFO", info: challengeList.error})
-    : dispatch({ type: CHALLENGE_LIST_SUCCESS, challengeList: challengeList.data });
-    dispatch({type: "AUTH_SUCCESS", user: user})
-  } else {
-    dispatch({ type: "NAVIGATE_TO_SIGN_IN" });
-  }
-};
-
-export const navigateToChallengeList = () => (dispatch) =>
-  dispatch({ type: NAVIGATE_TO_CHALLENGE_LIST });
-
 export const switchAuth = (to) => (dispatch) => {
   if (to === "SIGN_IN") {
     dispatch({ type: "NAVIGATE_TO_SIGN_IN" });
@@ -48,37 +22,16 @@ export const switchAuth = (to) => (dispatch) => {
 };
 
 export const authAction = (authData) => async (dispatch) => {
-  if (authData.type === "signIn") {
-    const action = await signIn(authData);
-    action.error 
-    ? dispatch({ type: "SET_INFO", info: action.error })
-    : dispatch(action);
-  } else {
-    const action = await signUp(authData);
-    action.error 
-    ? dispatch({ type: "SET_INFO", info: action.error }) 
-    : dispatch(action);
-  }
+  const user =
+    authData.type === "signIn" ? await signIn(authData) : await signUp(authData);
+
+  checkForErrorAndDispatch(dispatch, user, () =>
+    dispatch({ type: "AUTH_SUCCESS", user: user.data })
+  );
 };
 
 export const signOutAction = () => (dispatch) => {
   dispatch({ type: "SIGN_OUT" });
-};
-
-export const joinTeamAction = (joinData) => async (dispatch, getState) => {
-  const jwt = getState().userReducer.jwt;
-  const joinTeamResponse = await joinTeam(joinData, jwt);
-  joinTeamResponse.error
-  ? dispatch({type: "SET_INFO", info: joinTeamResponse.error})
-  : navigateToDetailsAction(joinData.challenge_id)(dispatch, getState);
-};
-
-export const createTeamAction = (createTeamData) => async (dispatch, getState) => {
-  const jwt = getState().userReducer.jwt;
-  const createTeamResponse = await createTeam(createTeamData, jwt);
-  createTeamResponse.error 
-  ? dispatch({type: "SET_INFO", info: createTeamResponse.error})
-  : navigateToDetailsAction(createTeamData.challenge_id)(dispatch, getState);
 };
 
 export const navigateToCreateChallengeAction = () => (dispatch) =>
@@ -86,24 +39,71 @@ export const navigateToCreateChallengeAction = () => (dispatch) =>
 
 export const createChallengeAction = (newChallengeData) => async (dispatch, getState) => {
   const jwt = getState().userReducer.jwt;
-  const response = await createChallenge(newChallengeData, jwt);
-  console.log(response)
-  response.error
-  ? dispatch({type: "SET_INFO", info: response.error})
-  : dispatch({ type: NAVIGATE_TO_CHALLENGE_LIST });
+  const createChallengeResponse = await createChallenge(newChallengeData, jwt);
+
+  checkForErrorAndDispatch(dispatch, createChallengeResponse, () =>
+    dispatch({ type: NAVIGATE_TO_CHALLENGE_LIST })
+  );
+};
+
+export const getChallengeListAction = () => async (dispatch, getState) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const jwt = getState().userReducer.jwt || (user && user.jwt);
+  const sortByData = { type: getState().challengeListReducer.sort };
+  if (jwt) {
+    const challengeList = await getChallengeList(jwt, sortByData);
+    checkForErrorAndDispatch(dispatch, challengeList, () =>
+      dispatch({ type: CHALLENGE_LIST_SUCCESS, challengeList: challengeList.data })
+    );
+    dispatch({ type: "AUTH_SUCCESS", user: user });
+  } else {
+    dispatch({ type: "NAVIGATE_TO_SIGN_IN" });
+  }
 };
 
 export const sortByAction = (sortByData) => async (dispatch, getState) => {
   const jwt = getState().userReducer.jwt;
   const challengeList = await getChallengeList(jwt, sortByData);
-  challengeList.error
-  ? dispatch({type: "SET_INFO", info: challengeList.error})
-  : dispatch({
-    type: "SORT_SUCCESS",
-    challengeList: challengeList.data,
-    sort: sortByData.type,
-  });
+  checkForErrorAndDispatch(dispatch, challengeList, () =>
+    dispatch({
+      type: "SORT_SUCCESS",
+      challengeList: challengeList.data,
+      sort: sortByData.type,
+    })
+  );
+};
+
+export const navigateToChallengeList = () => (dispatch) =>
+  dispatch({ type: NAVIGATE_TO_CHALLENGE_LIST });
+
+export const navigateToDetailsAction = (id) => async (dispatch, getState) => {
+  const jwt = getState().userReducer.jwt;
+  const challengeDetails = await getChallengeDetails(id, jwt);
+
+  checkForErrorAndDispatch(dispatch, challengeDetails, () =>
+    dispatch({ type: NAVIGATE_TO_CHALLENGE_DETAILS, challenge: challengeDetails.data })
+  );
+};
+
+export const createTeamAction = (createTeamData) => async (dispatch, getState) => {
+  const jwt = getState().userReducer.jwt;
+  const createTeamResponse = await createTeam(createTeamData, jwt);
+  checkForErrorAndDispatch(dispatch, createTeamResponse, () =>
+    navigateToDetailsAction(createTeamData.challenge_id)(dispatch, getState)
+  );
+};
+
+export const joinTeamAction = (joinData) => async (dispatch, getState) => {
+  const jwt = getState().userReducer.jwt;
+  const joinTeamResponse = await joinTeam(joinData, jwt);
+  checkForErrorAndDispatch(dispatch, joinTeamResponse, () =>
+    navigateToDetailsAction(joinData.challenge_id)(dispatch, getState)
+  );
 };
 
 export const setInfoAction = (info) => (dispatch) =>
   dispatch({ type: "SET_INFO", info: info });
+
+export const checkForErrorAndDispatch = (dispatch, data, callback) => {
+  data.error ? dispatch({ type: "SET_INFO", info: data.error }) : callback();
+};
--- src/api/challengeApi.js
+++ src/api/challengeApi.js
@@ -2,13 +2,36 @@ import axios from "axios";
 
 const URL = "http://localhost:4000";
 
-export const getChallengeDetails = async (id, jwt) => {
+export const signUp = async (authData) => {
   try {
-    return await axios.get(`${URL}/api/challenge/${id}`, {
+    return await axios.post(`${URL}/api/user/sign_up`, { user: authData });
+  } catch (error) {
+    return {
+      error:
+        "Failed to sign up. Check if fields are empty or email without @, min length 5 or already taken or Try later.",
+    };
+  }
+};
+
+export const signIn = async (authData) => {
+  try {
+    return await axios.post(`${URL}/api/user/sign_in`, { user: authData });
+  } catch (error) {
+    return {
+      error: "Failed to sign in. Check if fields are empty or invalid or Try later.",
+    };
+  }
+};
+
+export const createChallenge = async (newChallengeData, jwt) => {
+  try {
+    return await axios.post(`${URL}/api/challenge`, newChallengeData, {
       headers: { Authorization: `Bearer ${jwt}` },
     });
   } catch (error) {
-    return { error: "Failed to get Challenge Details. Try Later" };
+    return {
+      error: "Failed to create challenge. Check if fields are empty or Try Later.",
+    };
   }
 };
 
@@ -23,39 +46,13 @@ export const getChallengeList = async (jwt, sortByData) => {
   }
 };
 
-export const signIn = async (authData) => {
+export const getChallengeDetails = async (id, jwt) => {
   try {
-    const user = await axios.post(`${URL}/api/user/sign_in`, { user: authData });
-    return { type: "AUTH_SUCCESS", user: user.data };
-  } catch (error) {
-    return {
-      error: "Failed to sign in. Check if fields are empty or invalid or Try later.",
-    };
-  }
-};
-
-export const signUp = async (authData) => {
-  try {
-    const user = await axios.post(`${URL}/api/user/sign_up`, { user: authData });
-    return { type: "AUTH_SUCCESS", user: user.data };
-  } catch (error) {
-    return {
-      error:
-        "Failed to sign up. Check if fields are empty or email without @, min length 5 or already taken or Try later.",
-    };
-  }
-};
-
-export const joinTeam = async (joinData, jwt) => {
-  try {
-    return await axios.post(`${URL}/api/challenge/team/user`, joinData, {
+    return await axios.get(`${URL}/api/challenge/${id}`, {
       headers: { Authorization: `Bearer ${jwt}` },
     });
   } catch (error) {
-    return {
-      error:
-        "Failed to join team. You may have already joined the same team or Try later.",
-    };
+    return { error: "Failed to get Challenge Details. Try Later" };
   }
 };
 
@@ -69,14 +66,15 @@ export const createTeam = async (createTeamData, jwt) => {
   }
 };
 
-export const createChallenge = async (newChallengeData, jwt) => {
+export const joinTeam = async (joinData, jwt) => {
   try {
-    return await axios.post(`${URL}/api/challenge`, newChallengeData, {
+    return await axios.post(`${URL}/api/challenge/team/user`, joinData, {
       headers: { Authorization: `Bearer ${jwt}` },
     });
   } catch (error) {
     return {
-      error: "Failed to create challenge. Check if fields are empty or Try Later.",
+      error:
+        "Failed to join team. You may have already joined the same team or Try later.",
     };
   }
 };
